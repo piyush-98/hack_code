942b477de24812e3f63b198517d8b2102a01ef99
Jaspreet Singh <30948046+iamjaspreetsingh@users.noreply.github.com>
2019-01-09 00:14:04 +0530

    ylive

--- fire.py
+++ fire.py
@@ -1,4 +1,102 @@
-from urllib import request
+#!/usr/bin/python
+
+# Usage:
+# python sample.py --videoid='<video_id>'
+
+import httplib2
+import os
+import sys
+
+from apiclient.discovery import build_from_document
+from apiclient.errors import HttpError
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
 from firebase import firebase
 firebase=firebase.FirebaseApplication("https://lifesaver-18f28.firebaseio.com/")
-res=firebase.put("/dir","count","forward")  
+
+
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+YOUTUBE_READ_WRITE_SSL_SCOPE = "https://www.googleapis.com/auth/youtube.force-ssl"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+MISSING_CLIENT_SECRETS_MESSAGE = "MISSING_CLIENT_SECRETS_MESSAGE"
+
+os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+# Authorize the request and store authorization credentials.
+def get_authenticated_service(args):
+  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE,
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+  storage = Storage("%s-oauth2.json" % sys.argv[0])
+  credentials = storage.get()
+
+  if credentials is None or credentials.invalid:
+    credentials = run_flow(flow, storage, args)
+
+  # Trusted testers can download this discovery document from the developers page
+  # and it should be in the same directory with the code.
+  with open("youtube-v3-discoverydocument.json", "r") as f:
+    doc = f.read()
+    return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
+
+
+# Call the API's commentThreads.list method to list the existing comment threads.
+def get_comment_threads(youtube, video_id):
+  results = youtube.commentThreads().list(
+    part="snippet",
+    videoId=video_id,
+    textFormat="plainText"
+  ).execute()
+
+  for item in results["items"]:
+    comment = item["snippet"]["topLevelComment"]
+    author = comment["snippet"]["authorDisplayName"]
+    text = comment["snippet"]["textDisplay"]
+    print (author)
+    print (text)
+    
+   #res=firebase.put("/dir","count","forward")  
+    if "left" == text:
+      res=firebase.put("/","dir",1)  
+    elif "right" == text:
+      res=firebase.put("/","dir",8)  
+
+  return results["items"]
+
+
+# Call the API's comments.list method to list the existing comment replies.
+def get_comments(youtube, parent_id):
+  results = youtube.comments().list(
+    part="snippet",
+    parentId=parent_id,
+    textFormat="plainText"
+  ).execute()
+
+  for item in results["items"]:
+    author = item["snippet"]["authorDisplayName"]
+    text = item["snippet"]["textDisplay"]
+    print (author)
+    print (text)
+
+  return results["items"]
+
+
+if __name__ == "__main__":
+  # The "videoid" option specifies the YouTube video ID that uniquely
+  # identifies the video for which the comment will be inserted.
+  argparser.add_argument("--videoid",
+    help="Required; ID for video for which the comment will be inserted.")
+  args = argparser.parse_args()
+
+  if not args.videoid:
+    exit("Please specify videoid using the --videoid= parameter.")
+ 
+  youtube = get_authenticated_service(args)
+  video_comment_threads = get_comment_threads(youtube, args.videoid)
+  parent_id = video_comment_threads[0]["id"]
+  video_comments = get_comments(youtube, parent_id)
